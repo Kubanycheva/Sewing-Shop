395fe04bfd7749a93bad17b95a1ad8419b1f9d1d
BekturKochorbaev <bekturkochorbaev64@gmail.com>
2024-11-24 16:05:00 +0600

    cool

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest_framework',
     'shop'
 ]
 
--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Сatalog(models.Model):
+    name = models.CharField(max_length=52)
+
+
