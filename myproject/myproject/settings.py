3c95c338878be3f3f2882bb89674ec35a76ac54d
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2024-11-24 16:00:51 +0600

    shop

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest_framework',
     'shop'
 ]
 
@@ -134,3 +134,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'shop.User'
